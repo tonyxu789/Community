e3371035d132b82eddd53cd28dcd670b3998fe64
xuhuacheng <xuhuacheng@oppo.com>
2020-08-17 18:23:10 +0800

    08.17

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -1,4 +1,16 @@
 package com.nowcoder.community.util;
 
 public class RedisKeyUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+
+    // 某个实体的攒
+    // like:entity:entityType:entityId -> set(userId)
+    public static String getEntityLikeKey(int entityTpye, int entityId){
+        return PREFIX_ENTITY_LIKE + SPLIT + entityTpye + SPLIT + entityId;
+    }
+
+
+
 }
