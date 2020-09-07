f86251e6b3eb25c5e0dc2cc87c00e281c78a4ad9
xuhuacheng <xuhuacheng@oppo.com>
2020-09-07 11:31:06 +0800

    08.28

--- src/test/java/com/nowcoder/community/KafkaTests.java
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -18,35 +18,40 @@ public class KafkaTests {
 
     @Autowired
     private KafkaProducer kafkaProducer;
+
     @Test
-    public void testKafka(){
-        kafkaProducer.sendMessage("test","nihao");
-        kafkaProducer.sendMessage("test","zaima");
+    public void testKafka() {
+        kafkaProducer.sendMessage("test", "你好");
+        kafkaProducer.sendMessage("test", "在吗");
 
         try {
-            Thread.sleep(1000*10);
-        }catch (InterruptedException e){
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 }
 
 @Component
-class KafkaProducer{
+class KafkaProducer {
 
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
-    public void sendMessage(String topic, String content){
-        kafkaTemplate.send(topic,content);
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
     }
 
 }
 
 @Component
-class KafkaConsumer{
-    @KafkaListener(topics ={"test"})
-    public void handlerMessage(ConsumerRecord record){
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
         System.out.println(record.value());
     }
+
+
 }
